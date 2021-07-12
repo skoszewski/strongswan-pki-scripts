67a2beb39a2b8e31949758d1a3580ebc2ccd618d
Slawek Koszewski <slawek@koszewscy.waw.pl>
2021-07-12 12:23:29 +0200

    Added ability to specified own client pkcs12 file password.

--- client-cert.sh
+++ client-cert.sh
@@ -43,11 +43,25 @@ pki --gen --outform pem > $KEYPATH
 pki --issue --cacert $CACERT --cakey $CAKEY --in $KEYPATH --type priv --dn "E=$EMAIL,CN=$CN${CADNSUFFIX}" --flag clientAuth --crl $CRLURI --outform pem --digest sha256 > $CERTPATH
 echo "done."
 
-openssl rand 12 | base64 > $CERTDIR/$BASENAME.txt
-openssl pkcs12 -export -in $CERTPATH -inkey $KEYPATH -certfile $CACERT -passout file:$CERTDIR/$BASENAME.txt -name "$CN" -out $CERTDIR/$BASENAME.p12
-echo "The password for $BASENAME.p12 is \"$(cat $CERTDIR/$BASENAME.txt)\"."
-echo ""
+read -p "Do you want to provide you own export password for the PCKS#12 file? " ans
+
+if echo $ans | grep -q '^[yY]'
+then
+    openssl pkcs12 -export -in $CERTPATH -inkey $KEYPATH -certfile $CACERT -name "$CN" -out $CERTDIR/$BASENAME.p12
+else
+    openssl rand 12 | base64 > $CERTDIR/$BASENAME.txt
+    openssl pkcs12 -export -in $CERTPATH -inkey $KEYPATH -certfile $CACERT -passout file:$CERTDIR/$BASENAME.txt -name "$CN" -out $CERTDIR/$BASENAME.p12
+
+    echo "The password for $BASENAME.p12 is \"$(cat $CERTDIR/$BASENAME.txt)\"."
+    echo ""
+fi
+
 pki --print --in $CERTPATH
 echo ""
-echo "You will be asked to enter the following password: \"$(cat $CERTDIR/$BASENAME.txt)\""
+
+if [ -f $CERTDIR/$BASENAME.txt ]
+then
+    echo "You will be asked to enter the following password: \"$(cat $CERTDIR/$BASENAME.txt)\""
+fi
+
 pki --pkcs12 --list --in $CERTDIR/$BASENAME.p12
